f6274fe52f6add4ce97c55b9fd5fc99e7a77fea2
VoltBit <andreidobre.web@gmail.com>
2014-11-12 09:43:34 +0200

    T1

--- T1/TuringMachine.c
+++ T1/TuringMachine.c
@@ -15,15 +15,15 @@ typedef struct {
 	int moveDirection;
 } Transition;
 
-#define LEFT_BOUNDARY 0
-#define BAND_END 3
+#define LEFT_BOUNDARY '>'
+#define BAND_END '#'
 
 #define True 1
 #define False 0
 
 /* the alpahbet:
-	sig = { >, 0, 1, #}
-	sig will be represented in the input file like this:
+	sigma = { >, 0, 1, #}
+	sigma will be represented in the input file like this:
 	> -> 
 */
 
@@ -41,11 +41,11 @@ typedef struct {
 #define S5 5
 #define S6 6
 #define S7 7
-#define Success 8
-#define Fail 9
+#define Success 100
+#define Fail 101
 
-int states[] = {0,1,2,3,4,5,6,7}; // non-final states
-char sig[] = ">01#";
+int statesNumber; // number of non-final states
+Character sigma[255];
 Character band[MAX_BAND_LENGTH];
 State currentState = S0;
 int currentIndex = 1;
@@ -57,28 +57,26 @@ int convert(char x){
 		function that creates a relationship between alphabet characters
 		and memory representation
 		note that alphabet order si important
-		sig = { >, 0, 1, #} 
+		sigma = { >, 0, 1, #} 
 	*/
 	int i;
-	for(i = 0; i < strlen(sig); i++){
-		if(x == sig[i])return i;
+	for(i = 0; i < strlen(sigma); i++){
+		if(x == sigma[i])return i;
 	}
 	printf("\n\nConvert error: %c\n\n",x);
 	return -1;
 }
 
-static void initTransitionTable() {
-
-}
-
 static void initBand() {
 	int currentIndex = 1;
 
-	band[0] = LEFT_BOUNDARY;
+	band[0] = BAND_END;
+	band[1] = LEFT_BOUNDARY;
+
 	band[MAX_BAND_LENGTH-1] = BAND_END;
 }
 
-static Bool makeTransition() {
+static Bool makeTransition(int x) {
 	// The machine went out of band or reached a Fail state
 	if (currentIndex < 0 || currentIndex >= MAX_BAND_LENGTH ||
 		currentState == Fail)
@@ -87,46 +85,47 @@ static Bool makeTransition() {
 	if (currentState == Success)
 		return True;
 	Transition currentTransition;
-	if(convert(band[currentIndex])!=-1)
+	if(convert(band[currentIndex])!=-1) /* checks if conversion from char to index is possible */
 		currentTransition = transitionTable[currentState][convert(band[currentIndex])];
 	else return 0;
-
 	transitionsMade++;
 	currentState = currentTransition.nextState;
 	band[currentIndex] = currentTransition.toWrite;
 	currentIndex += currentTransition.moveDirection;
-
-	return makeTransition();
+	printf("%i ",x);
+	return makeTransition(++x);
 }
 
 void insertVal(char *file){
-	int i = 0,j,q;
+	int i = 1,j,q;
 	char dump;
 	FILE *f = fopen(file,"r");
 	do{
 		fscanf(f,"%c",&band[i]);
-		printf("%c",band[i]);
 		i++;
 	}while(band[i - 1] != '\n');
 	band[i] = '\0';
 	i = 0;
+	fgets(sigma,255,f);
 	while(!feof(f)){
 		q = 0;
-		for(j = 0; j < 3 * strlen(sig); j+=3){
+		for(j = 0; j < 3 * (strlen(sigma) -1); j+=3){
 			fscanf(f,"%i",&transitionTable[i][q].nextState);
+			fscanf(f,"%c",&dump); /* remove space */
 			fscanf(f,"%c",&transitionTable[i][q].toWrite);
+			fscanf(f,"%c",&dump); /* remove space */
 			fscanf(f,"%i",&transitionTable[i][q].moveDirection);
 			q++;
 		}
 		i++;
-		fscanf(f,"%c",&dump);
+		fscanf(f,"%c",&dump); /* remove endline */
 	}
+	statesNumber = i;
 }
 
 void display(){
 	int i = 0,j,q;
-	int statesNumber = sizeof(states)/sizeof(states[0]);
-	printf("Band: %i\n",statesNumber);
+	printf("Band: \n");
 	while(band[i]){
 		printf("%c",band[i++]);
 	}
@@ -134,7 +133,7 @@ void display(){
 	printf("\nTransitions: \n");
 	for(i = 0; i < statesNumber; i++){
 		q = 0;
-		for(j = 0; j < strlen(sig); j++){
+		for(j = 0; j < strlen(sigma) - 1; j++){
 			printf("%i ",transitionTable[i][q].nextState);
 			printf("%c ",transitionTable[i][q].toWrite);
 			printf("%i ",transitionTable[i][q].moveDirection);
@@ -146,8 +145,8 @@ void display(){
 
 int main(void) {
 	initBand();
-	insertVal("data.in");
-	Bool outcome = makeTransition();
+	insertVal("data2.in");
+	Bool outcome = makeTransition(0);
 	printf("MT made %ld transitions, and reached %s state.\n",
 		transitionsMade, outcome ? "SUCCESS" : "FAIL");
 	display();
