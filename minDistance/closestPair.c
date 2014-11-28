4cf116ff96e3bed873dad176e9f80f63321fa427
VoltBit <andreidobre.web@gmail.com>
2014-11-28 20:35:03 +0200

    H2

--- minDistance/closestPair.c
+++ minDistance/closestPair.c
@@ -32,12 +32,17 @@ int minStripDistance1(Point strip[], int size, float d, Point &p1, Point &p2){
 	float min = d;
 	int i,j;
 	float aux;
+
+	qsort(strip,size,sizeof(Point),sortY);
+
 	for(i = 0; i < size; i++){
 		for(j = i+1; j < size && (strip[j].y - strip[i].y) < min; j++){
 			/*
 				Punctele din banda sunt ordonate dupa y deci daca distanta
 				minima este depasita pe axa y nu se continua verificarea pentru
 				punctul curent.
+				Este demonstrat matematic ca in banda nu pot fi mai mult de 6
+				puncte.
 			*/
 			aux = getDistance(strip[i],strip[j]);
 			if( aux < min ){
@@ -76,13 +81,13 @@ float closestPair(Point points[], Point strip[], int pointNumber, Point &p1, Poi
 	*/
 	Point aux1, aux2, aux3, aux4, midPoint;
 	int i, j = 0, mid = pointNumber / 2;
-	float right,left,minDistance;
+	float right,left,minDistance = 99999;
 	midPoint = points[mid];
 	if(pointNumber <= 3){
 		/*
 			Caz in care nu pot creea benzi si verifica direct.
 		*/
-			return minStripDistance2(points,pointNumber,aux1,aux2);
+			return minStripDistance2(points,pointNumber,minDistance,aux1,aux2);
 	}
 	right = closestPair(points,strip,pointNumber,aux1,aux2);
 	left = closestPair(points + mid,strip,pointNumber,aux3,aux4);
@@ -99,20 +104,46 @@ float closestPair(Point points[], Point strip[], int pointNumber, Point &p1, Poi
 		p2.y = aux2.y;
 		minDistance = left;
 	}
+	/*
+		Creez o banda in vecinatatea punctului de mijloc care contine
+		punctele cu o distanta mai mica de minDistance, apoi caut cea
+		mai mica distanta din banda.
+	*/
 	for(i = 0; i < pointNumber; i++){
 		if(abs(points[i].x - midPoint.x) < minDistance){
 			strip[j] = points[i];
 			j++;
 		}
 	}
+	float aux = minStripDistance1(strip,j,minDistance,aux1,aux2);
+	if(aux < minDistance){
+		p1.x = aux1.x;
+		p1.y = aux1.y;
+		p2.x = aux2.x;
+		p2.y = aux2.y;
+		return aux;
+	}else{
+		return minDistance;
+	}
 }
 
 void findClosest(){
-	
+	Point points[100],strip[100];
+	FILE *f = fopen("data.in");
+	int i = 0;
+	while(!feof(f)){
+		fscanf(f,"%i",&points[i].x);
+		fscanf(f,"%i",&points[i].y);
+		i++;
+	}
+	Point p1,p2;
+	qsort(points,i,sizeof(Point),sortX);
+	printf("Closest distance: %.3f\n",closestPair(points,i,p1,p2));
+	printf("points: A(%i,%i),B(%i,%i)",p1.x,p1.y,p2.x,p2.y);
 }
 
-int main(){
 
+int main(){
 	findClosest();
 	return 0;
 }
